ee5a7096194c0f6f10fad27d01aa82defdfe4d18
Michelle Truong <42843557+Michelle-T@users.noreply.github.com>
2019-10-02 19:19:45 -0400

    Creation of Scenes
    Created MainMenu, Level1, and Level2 scenes as well as the start of the Menu script. Need to download SDK for Android on home computers.

---
+++ Eco_Aquatic_Unity/Assets/Scripts/Menu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Button newGameButton;
+    public string newGameSceneName;
+    public bool isPlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseUp()
+    {
+        /*if (isQuit)
+        {
+            //Application.Quit();
+        }*/
+        if (isPlay)
+        {
+            NewGame();
+        }
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameSceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("has quit game");
+        Application.Quit();
+    }
+}
