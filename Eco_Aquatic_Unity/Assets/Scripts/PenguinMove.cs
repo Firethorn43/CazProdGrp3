690843b198a57b22c352dfecd31c72539a482c45
Firethorn43 <37163317+Firethorn43@users.noreply.github.com>
2019-10-20 23:01:34 -0400

    Moving penguin phase

---
+++ Eco_Aquatic_Unity/Assets/Scripts/PenguinMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinMove : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    int current = 0;
+    float rotSpeed;
+    public float speed;
+    float WPradius = 1;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+      if(Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        {
+            current++;
+            if (current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+    }
+}
