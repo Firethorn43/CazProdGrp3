f1d09ab358ebe647218ca92b0e57f6e3ad9e2e8c
Firethorn43 <37163317+Firethorn43@users.noreply.github.com>
2019-10-23 14:13:32 -0400

    Penguin Movement Script Works

---
+++ Eco_Aquatic_Unity/Assets/UIMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class UIMove : MonoBehaviour
+
+  
+{
+    public GameObject[] waypoints;
+    int current = 0;
+    float rotSpeed;
+    public float speed;
+    float WPradius = 1;
+    float directionY;
+    Rigidbody2D rb; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        directionY = CrossPlatformInputManager.GetAxis("Vertical");
+        rb.velocity = new Vector2(0, directionY * 10);
+
+        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        {
+            current++;
+            if (current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+    }
+}
